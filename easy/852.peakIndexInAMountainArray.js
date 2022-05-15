67d0369478568c7d76f88aefaf76dfa63625716e
Jay Pan <lb0104672@gmail.com>
2022-05-16 00:26:20 +0800

    easy: 852. Peak Index in a Mountain Array

---
+++ easy/852.peakIndexInAMountainArray.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+// O(N)
+var peakIndexInMountainArrayIterative = function(arr) {
+  let i = 0;
+  
+  while(arr[i] < arr[i + 1]) {
+      i += 1;
+  }
+  
+  return i;
+};
+
+// O(logN)
+//  解題思路： 根據 arr[i] < arr[i + 1] 的規則使用 divide and conquer，最終 left === right 而跳出 while loop
+var peakIndexInMountainArray = function(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  
+  while(left < right) {
+      const middleIdx = Math.floor((left + right) / 2);
+      
+      if(arr[middleIdx] < arr[middleIdx + 1]) {
+          left = middleIdx + 1    
+      } else { // means current right index is the biggest
+          right = middleIdx;
+      }
+  }
+  
+  return left;
+}
