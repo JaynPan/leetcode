6a102b12087dc764cd09e8176027ced6a2117f9d
Jay Pan <lb0104672@gmail.com>
2022-06-26 00:44:47 +0800

    refactor: optimize 13.

--- easy/13.romanToInteger.js
+++ easy/13.romanToInteger.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 
-var romanToInt = function(s) {
+ var romanToInt = function(s) {
   let sum = 0;
   const map = {
       "I": 1,
@@ -13,18 +13,23 @@ var romanToInt = function(s) {
       "C": 100,
       "D": 500,
       "M": 1000,
+      "IV": 4,
+      "IX": 9,
+      "XL": 40,
+      "XC": 90,
+      "CD": 400,
+      "CM": 900
   }
   
   let i = s.length - 1;
   
   while(i >= 0) {
       const character = s[i];
+      // the purpose of pair is to check whether it is subtraction cases
+      const pair = map[s.substring(i - 1, i + 1)];
       
-      if(((character === 'V' || character === 'X') && s[i-1] === 'I') ||
-         ((character === 'L' || character === 'C') && s[i-1] === 'X') ||
-         ((character === 'D' || character === 'M') && s[i-1] === 'C')
-        ) {
-          sum += map[s[i]] - map[s[i -1]];
+      if(i > 0 && pair) {
+          sum += pair;
           i-= 2;
       } else {
           sum += map[s[i]];
