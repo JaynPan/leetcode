a56fbe2631a59a4fe0e3dd62293edcb5bde3229d
Jay Pan <lb0104672@gmail.com>
2022-07-06 18:19:18 +0800

    744. optimize bs

--- easy/744.findSmallestLetterGreaterThanTarget.js
+++ easy/744.findSmallestLetterGreaterThanTarget.js
@@ -25,26 +25,19 @@ var nextGreatestLetter = function(letters, target) {
 
 // * Binary Search: Divide and conquer
 // Since it is sorted array, we can use divide and conquer technique to find the value that is greater than target
-var nextGreatestLetterDivideConquer = function(letters, target) {
-  let left = 0;
-  let right = letters.length - 1;
-  
-  // find the insert position
-  while(left < right) {
-      const pivot = Math.floor((left + right) / 2)
+var nextGreatestLetter = function(letters, target) {
+    let left = 0;
+    let right = letters.length - 1;
+    
+    while(left <= right) {
+        const pivot = Math.floor((left + right) / 2)
 
-      if(letters[pivot] <= target) {
-          left = pivot + 1;
-      } else {
-          right = pivot;
-      }
-  }
-  
-  // when the target is greater or equal to insert position letter
-  // the right position value will be the smallest value that is larger than target 
-  if(letters[left] <= target) {
-      return letters[left + 1] || letters[0]
-  } else {
-      return letters[left]
-  }
+        if(letters[pivot] <= target) {
+            left = pivot + 1;
+        } else {
+            right = pivot - 1;
+        }
+    }
+    
+    return left >= letters.length ? letters[0] : letters[left];
 };
