66d7c827795954f7b863b8c565984d601dcd64d9
Jay Pan <lb0104672@gmail.com>
2022-07-12 17:39:45 +0800

    linkedlist: 19. Remove Nth Node From End of List

---
+++ medium/19.removeNthNodeFromEndOfList.js
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+
+// * Reversed linked lis
+// Time: O(3N)
+// Space: O(1)
+var removeNthFromEnd = function(head, n) {
+  const reversedLinkedList = (node) => {
+    let previous = null;
+    let currentNode = node;
+    
+    while(currentNode) {
+      const temp = currentNode.next;
+      currentNode.next = previous;
+      previous = currentNode;
+      currentNode = temp;  null
+    }
+    
+    return previous;
+  }
+  
+  let reversed = reversedLinkedList(head);
+
+  // remove nth node in a reveredLinkedList
+  let counter = 1;
+  let previousNode = null;
+  let currentNode = reversed;
+
+  while(counter <= n) {
+    if (counter === n) {
+      // edge case: when n = 1
+      if(!previousNode) {
+        reversed = currentNode.next; // null
+      } else {
+        previousNode.next = currentNode.next;          
+      }
+
+      break;  
+    }
+    
+    previousNode = currentNode;
+    currentNode = currentNode.next;
+    counter++;
+  }
+
+  // reversed the reversed linked list
+  return reversedLinkedList(reversed);
+};
+
+// * One Pass
+// Time: O(2N)
+// Space: O(1)
+var removeNthFromEnd = function(head, n) {
+  let length = 0;
+  let currentNode = head;
+  
+  while(currentNode) {
+    currentNode = currentNode.next;
+    length++;
+  }
+  
+  let nodeBeforeRemoveIdx = length - n - 1;
+
+  if(nodeBeforeRemoveIdx < 0) {
+    return head.next;
+  }
+  
+  currentNode = head;
+  
+  for(let i = 0; i < nodeBeforeRemoveIdx; i++) {
+    currentNode = currentNode.next;
+  }
+  
+  currentNode.next = currentNode.next.next;
+  
+  return head;
+}; 
