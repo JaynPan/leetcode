2e8bac5b7ec196f89669943292c8f8ae01859d3e
Jay Pan <lb0104672@gmail.com>
2022-07-24 19:32:04 +0800

    hashmap: 128

--- medium/128.longestConsecutiveSequence.js
+++ medium/128.longestConsecutiveSequence.js
@@ -26,3 +26,31 @@ var longestConsecutive = function(nums) {
   
   return max;
 };
+
+// * Array, Hash map
+// Time: O(N)
+// Space: O(N)
+var longestConsecutive = function(nums) {
+  const set = new Set();
+  let max = 0;
+  
+  for (const num of nums) {
+    set.add(num);
+  }
+  
+  for(const int of set) {
+    // the number is the begining of the sequence
+    if(!set.has(int - 1)) {
+      let currentInt = int;
+      let count = 0;
+
+      while(set.has(currentInt)) {
+        count++;
+        currentInt++;
+        max = Math.max(count, max);
+      }
+    }
+  }
+  
+  return max;
+};
